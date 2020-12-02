766f1ececa65b1dc8b8630c9df65d39f28f22626
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2020-12-02 17:09:57 +0500

    Added Brand logo and Brand Name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      {/* company logo and name  */}
+      <div className="row h-25">
+        <div className="col d-flex flex-row">
+          <div className="container-fluid h-50  d-flex m-0 p-0">
+          <img
+            src={Logo}
+            className="img-responsive h-50 justify-content-start"
+            alt=""
+          />
+          <h1 style={{color:'#B1ADAD',marginTop:'20px',fontFamily: 'Sansita Swashed cursive',userSelect:'none'}}>Digital Luxe</h1>
+          </div>
+          
+          
+        </div>
+      </div>
     </div>
   );
 }
